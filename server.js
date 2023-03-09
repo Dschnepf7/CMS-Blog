3d88f9d0b4081bd8c324fd934cfec445459b1ff1
Dschnepf7 <domschnepf1@gmail.com>
2023-03-08 21:46:07 -0600

    began working on routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -32,5 +32,5 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening at http://localhost${PORT}`));
 });
